420aa504a7ec2505176378c38c845e7fa66d135b
Shenn <yshen0505@gmail.com>
2025-12-05 21:51:12 +0800

    Add Firebase integration and configuration for the project

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -35,6 +38,15 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    signingConfigs {
+        debug {
+            storeFile file(MYAPP_DEBUG_STORE_FILE)
+            storePassword MYAPP_DEBUG_STORE_PASSWORD
+            keyAlias MYAPP_DEBUG_KEY_ALIAS
+            keyPassword MYAPP_DEBUG_KEY_PASSWORD
         }
     }
 }
