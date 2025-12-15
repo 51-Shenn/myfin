13e884579a50b679eeac0fac9e9bb027c92e17c5
briankdx <briankdx-wm24@student.tarc.edu.my>
2025-12-15 22:59:05 +0800

    feat: Implement report generators for accounts payable, accounts receivable, balance sheet, cash flow, and profit & loss reports

--- lib/features/report/services/generator/acc_receivable_generator.dart
+++ lib/features/report/services/generator/acc_receivable_generator.dart
@@ -1,1 +1,35 @@
 // prepare account receivable report data for page viewing
+
+import 'package:myfin/features/report/services/generator/report_generator_interface.dart';
+import 'package:myfin/features/report/domain/entities/report.dart';
+import 'package:myfin/features/report/services/generator/report_template.dart';
+import 'package:myfin/features/upload/domain/entities/doc_line_item.dart';
+
+class AccReceivableGenerator implements ReportGeneratorInterface {
+  final template = AccountsReceivableTemplate();
+
+  @override
+  Report generateFullReport(Report report, String businessName, List<DocumentLineItem> reportData) {
+    // TODO: implement generateFullReport
+    throw UnimplementedError();
+  }
+
+  @override
+  ReportGroup generateGroup(String groupName) {
+    // TODO: implement generateGroup
+    throw UnimplementedError();
+  }
+
+  @override
+  ReportLineItem generateItem(String itemName, DocumentLineItem itemData) {
+    // TODO: implement generateItem
+    throw UnimplementedError();
+  }
+
+  @override
+  ReportSection generateSection(String sectionName) {
+    // TODO: implement generateSection
+    throw UnimplementedError();
+  }
+
+}
--- lib/features/report/services/generator/profitloss_generator.dart
+++ lib/features/report/services/generator/profitloss_generator.dart
@@ -11,55 +11,35 @@
 //  add new section to list
 // return report data model with sections list
 
-import 'package:myfin/features/report/domain/entities/report.dart';
 import 'package:myfin/features/report/services/generator/report_generator_interface.dart';
+import 'package:myfin/features/report/domain/entities/report.dart';
+import 'package:myfin/features/report/services/generator/report_template.dart';
+import 'package:myfin/features/upload/domain/entities/doc_line_item.dart';
 
 class ProfitLossGenerator implements ReportGeneratorInterface {
+  final template = ProfitAndLossTemplate();
+
   @override
-  Report generateFullReport(Map<String, dynamic> reportData) {
+  Report generateFullReport(Report report, String businessName, List<DocumentLineItem> reportData) {
     // TODO: implement generateFullReport
     throw UnimplementedError();
   }
 
   @override
-  List<double> generateGrandTotal(double amount) {
-    // TODO: implement generateGrandTotal
+  ReportGroup generateGroup(String groupName) {
+    // TODO: implement generateGroup
     throw UnimplementedError();
   }
 
   @override
-  List<String> generateGroupHeader(String groupName) {
-    // TODO: implement generateGroupHeader
+  ReportLineItem generateItem(String itemName, DocumentLineItem itemData) {
+    // TODO: implement generateItem
     throw UnimplementedError();
   }
 
   @override
-  List<String> generateItemHeader(String itemName, {Map<String, dynamic>? itemData}) {
-    // TODO: implement generateItemHeader
-    throw UnimplementedError();
-  }
-
-  @override
-  List<String> generateReportTitle(Report report, String businessName) {
-    // TODO: implement generateReportTitle
-    throw UnimplementedError();
-  }
-
-  @override
-  List<String> generateSectionHeader(String sectionName) {
-    // TODO: implement generateSectionHeader
-    throw UnimplementedError();
-  }
-
-  @override
-  List<double> generateSubtotal(String groupName, double amount) {
-    // TODO: implement generateSubtotal
-    throw UnimplementedError();
-  }
-
-  @override
-  List<double> generateTotal(String sectionName, double amount) {
-    // TODO: implement generateTotal
+  ReportSection generateSection(String sectionName) {
+    // TODO: implement generateSection
     throw UnimplementedError();
   }
 
