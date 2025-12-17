c921a10dc5fde890c25debb667c8c6ade6aa9f94
Shenn <yshen0505@gmail.com>
2025-12-18 04:11:44 +0800

    feat: Add multi-platform app icons and initial Flutter application entry point.

--- lib/main.dart
+++ lib/main.dart
@@ -56,7 +56,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(useMaterial3: true),
 
       // home: const BottomNavBar(),
